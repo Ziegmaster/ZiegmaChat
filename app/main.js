04a9d715a57303b4a14d036f175d5f2980c6dfa5
trueziegmaster <trueziegmaster@gmail.com>
2023-11-16 09:42:51 +0500

    Polished some code in main.js

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
-// Modules to control application life and create native browser window
 const { app, Menu, Tray, BrowserWindow, ipcMain } = require('electron');
 const windowStateKeeper = require('electron-window-state');
 const path = require('path');
 const settings = require('./settings');
 const server = require('./server');
 
+//START INTERNAL HTTP SERVER
 server.listen(settings.get('app.server.port'));
 
 const iconPath = path.join(__dirname, 'favicon.ico');
@@ -15,6 +15,7 @@ let chatWindow;
 let mainWindowState;
 let chatWindowState;
 
+//GENERATE WIDGET URL FOR FURTHER USAGE AND EXPORT
 const getChatURL = () => {
   let s = settings.get('app');
   let url = 'http://localhost:';
@@ -24,112 +25,124 @@ const getChatURL = () => {
   return url;
 };
 
-const createWindow = (type) => {
+//MAIN WINDOW
+const createMainWindow = (type) => {
   mainWindow = new BrowserWindow({
-    x : mainWindowState.x,
-    y : mainWindowState.y,
-    width : 420,
-    height : 440,
-    icon : iconPath,
-    resizable : false,
-    webPreferences : {
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: 420,
+    height: 440,
+    icon: iconPath,
+    resizable: false,
+    webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
   mainWindow.removeMenu();
+  //Hide to tray instead of minimizing
   mainWindow.on('minimize', function (event) {
     event.preventDefault();
     mainWindow.hide();
   });
+  //Close entire app
   mainWindow.on('closed', function (event) {
-    try{
-      chatWindow.destroy();
-    }
-    catch{}
+    mainWindow = null;
+    chatWindow?.close();
     app.quit();
-  })
+  });
   mainWindow.loadFile('index.html');
   mainWindowState.manage(mainWindow);
 }
 
+//CHAT WINDOW
 const createChatWindow = () => {
+  //This is not a window's settings but the widget one's
   let chat_settings = settings.get('app.chat');
-  chatWindow?.destroy();
+  chatWindow?.close();
   chatWindow = new BrowserWindow({
-    x : chatWindowState.x,
-    y : chatWindowState.y,
-    width : chatWindowState.width,
-    height : chatWindowState.height,
-    transparent : chat_settings.game_mode,
-    frame : !chat_settings.game_mode,
-    icon : iconPath,
+    x: chatWindowState.x,
+    y: chatWindowState.y,
+    width: chatWindowState.width,
+    height: chatWindowState.height,
+    transparent: chat_settings.game_mode,
+    frame: !chat_settings.game_mode,
+    icon: iconPath,
   });
   chatWindow.removeMenu();
   chatWindow.setAlwaysOnTop(chat_settings.game_mode, 'screen-saver');
   chatWindow.setIgnoreMouseEvents(chat_settings.game_mode);
-  if (chat_settings.game_mode){
+  //Some dark magic to fix Electron window's header bug with frameless window
+  if (chat_settings.game_mode) {
     chatWindow.on('blur', () => {
-      try{
-        chatWindow.setBackgroundColor('#00000000');
-      }
-      catch{}
+      chatWindow.setBackgroundColor('#00000000');
     });
     chatWindow.on('focus', () => {
-      try{
-        chatWindow.setBackgroundColor('#00000000');
-      }
-      catch{}
+      chatWindow.setBackgroundColor('#00000000');
     });
   }
+  //Send "closed" event to the main window when the window is actually closed
   chatWindow.on('closed', function () {
-    try{
-      mainWindow.webContents.send('chat-closed');
-    }
-    catch{}
     chatWindow = null;
+    mainWindow?.webContents.send('chat-closed');
   });
   chatWindow.loadURL(getChatURL());
   chatWindowState.manage(chatWindow);
+  //Send "opened" event to the main window
   mainWindow.webContents.send('chat-opened');
 }
 
-//ipc
+//IPC SECTION
+
+//Create chat window when receive command
 ipcMain.on('chat-open', createChatWindow);
 
+//Close chat window when receive command
 ipcMain.on('chat-close', () => {
-  chatWindow?.destroy();
+  chatWindow.close();
 });
 
+//Copy chat URL to clipboard when receive command
 ipcMain.on('copy', (event, data) => {
   event.reply('copy', getChatURL());
 });
 
+//Get stored app settings when receive command
 ipcMain.on('settings-get', (event, data) => {
   event.reply('settings-get', settings.get('app'));
 });
 
+//Update stored app settings when receive command
 ipcMain.on('settings-set', (event, data) => {
-  console.log(data);
   let server_settings = settings.get('app.server');
-  settings.set('app.server', {...server_settings, ...data.server});
+  settings.set('app.server', { ...server_settings, ...data.server });
   let chat_settings = settings.get('app.chat');
-  settings.set('app.chat', {...chat_settings, ...data.chat});
-  if (data.server.port){
+  settings.set('app.chat', { ...chat_settings, ...data.chat });
+  if (data.server.port) {
     server.close();
     server.listen(data.server.port);
   }
-  if (chatWindow && Object.keys(data.chat).length > 0){
+  if (chatWindow && Object.keys(data.chat).length > 0) {
     createChatWindow();
   }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+//ELECTRON MAIN
 app.whenReady().then(() => {
 
-  tray = new Tray(iconPath);
+  //Create state files to save windows' size and position on reload
+  mainWindowState = windowStateKeeper({
+    file: 'main.json',
+  });
+  chatWindowState = windowStateKeeper({
+    file: 'chat.json',
+    defaultWidth: 800,
+    defaultHeight: 600,
+  });
 
+  createMainWindow();
+
+  //Create tray and its context menu
+  tray = new Tray(iconPath);
   const contextMenu = Menu.buildFromTemplate([
     {
       label: 'ZiegmaChat for Streamer.bot',
@@ -145,45 +158,24 @@ app.whenReady().then(() => {
     },
     {
       label: 'Quit', click: function () {
-        chatWindow?.destroy();
-        mainWindow.destroy();
+        chatWindow.close();
+        mainWindow.close();
         app.quit();
       }
     }
   ]);
-
   tray.setToolTip('ZiegmaChat');
   tray.setContextMenu(contextMenu);
-
-  mainWindowState = windowStateKeeper({
-    file : 'main.json',
-  });
-  
-  chatWindowState = windowStateKeeper({
-    file : 'chat.json',
-    defaultWidth : 800,
-    defaultHeight : 600,
-  });
-
-  createWindow();
-
   tray.on('click', () => {
     mainWindow.show();
   });
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
   })
-})
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+//APP CLOSURE
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+});
